997e0e1778c68cb71b54ecd7d49e985d2e101cc6
Gus Ruben <gusruben@protonmail.com>
2025-09-16 16:41:06 -0400

    Fix favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import { Toaster } from 'svelte-sonner';
 	import { onMount } from 'svelte';
 	import { initLocale } from '$lib/i18n';
@@ -13,7 +12,7 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href="/favicon.png" />
 </svelte:head>
 
 <Toaster position="top-right" />
